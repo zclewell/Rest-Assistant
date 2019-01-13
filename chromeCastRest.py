b5ef57372fc893c0a08b9391fe3b31fe7d8208c1
zclewell <zclewel@gmail.com>
2019-01-13 17:48:46 -0600

    Added support for dynamic control of chromecasts/actions, moved chromecast controls to chromeCastRest.py

---
+++ chromeCastRest.py
@@ -0,0 +1,81 @@
+import pychromecast
+from flask_restful import Resource
+
+UNKNOWN_STATE = 'UNKNOWN'
+PLAYING_STATE = 'PLAYING'
+PAUSED_STATE = 'PAUSED'
+
+TOGGLE_ACTION = 'toggle'
+PAUSE_ACTION = 'pause'
+PLAY_ACTION = 'play'
+STOP_ACTION = 'stop'
+
+mc_dict = {}
+for cc in pychromecast.get_chromecasts():
+    mc_dict[cc.device.friendly_name] = cc.media_controller
+print(len(mc_dict), 'Chromecasts found')
+
+
+class ToggleHomeTv(Resource):
+    def get(self):
+        # Get the media controller from the dict
+        mc = mc_dict['My TV']
+
+        # Can't call immediately for some reason, loop until we get response
+        while mc.status.player_state == 'UNKNOWN':
+            continue
+
+        if mc.status.player_state == 'PAUSED':
+            mc.play()
+        elif mc.status.player_state == 'PLAYING':
+            mc.pause()
+        return mc.status.player_state
+
+
+action_set = set(['play', 'pause', 'toggle', 'stop'])
+
+class HandleChromecast(Resource):
+    def get(self, name, action):
+        name = name.replace('_', ' ')
+        if name in mc_dict:
+            mc = mc_dict[name]
+        else:
+            return 'Name: ' + name + ' not found'
+
+        low_action = action.lower()
+        if low_action in action_set:
+            if low_action == TOGGLE_ACTION:
+                toggleMc(mc)
+            elif low_action == PAUSE_ACTION:
+                pauseMc(mc)
+            elif low_action == PLAY_ACTION:
+                playMc(mc)
+            elif low_action == STOP_ACTION:
+                stopMc(mc)
+            return 'Ran '+action+' on '+name
+        else:
+            return 'Action: ' + action + ' not found'
+
+def waitUntilKnown(mc):
+    while mc.status.player_state == UNKNOWN_STATE:
+        continue
+
+def toggleMc(mc):
+    waitUntilKnown(mc)
+    if mc.status.player_state == PAUSED_STATE:
+        mc.play()
+    elif mc.status.player_state == PLAYING_STATE:
+        mc.pause()
+    return mc.status.player_state
+
+def stopMc(mc):
+    waitUntilKnown(mc)
+    mc.stop()
+
+def playMc(mc):
+    waitUntilKnown(mc)
+    mc.play()
+
+def pauseMc(mc):
+    waitUntilKnown(mc)
+    mc.pause()
--- server.py
+++ server.py
@@ -1,35 +1,12 @@
-from flask import Flask
-from flask_restful import Resource, Api
-import pychromecast
+from flask import Flask, request
+from flask_restful import Api
+from chromeCastRest import HandleChromecast
 
 app = Flask(__name__)
 api = Api(app)
 
-mc_dict = {}
-for cc in pychromecast.get_chromecasts():
-	mc_dict[cc.device.friendly_name] = cc.media_controller
+api.add_resource(HandleChromecast, '/chromecast/<string:name>/<string:action>')
 
-
-class HelloWorld(Resource):
-    def get(self):
-        return {'hello': 'world'}
-
-class ToggleHomeTv(Resource):
-	def get(self):
-		# Get the media controller from the dict
-		mc = mc_dict['Home TV']
-
-		# Can't call immediately for some reason, loop until we response
-		while mc.status.player_state == 'UNKNOWN':
-			continue
-
-		if mc.status.player_state == 'PAUSED':
-			mc.play()
-		elif mc.status.player_state == 'PLAYING':
-			mc.pause()
-		return mc.status.player_state
-
-api.add_resource(ToggleHomeTv, '/toggle')
 
 if __name__ == '__main__':
     app.run(debug=True)

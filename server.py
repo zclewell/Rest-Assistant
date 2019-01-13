346acd49533fd50997f89f3ccf744bd86b2b804d
zclewell <zclewel@gmail.com>
2019-01-12 21:06:23 -0600

    cleaned up code

--- server.py
+++ server.py
@@ -1,18 +1,14 @@
 from flask import Flask
 from flask_restful import Resource, Api
-from requests import put, get
 import pychromecast
-import time
 
 app = Flask(__name__)
 api = Api(app)
-test = 'yeet'
-cc_list = pychromecast.get_chromecasts()
-for cc in cc_list:
-	# print(test)
-	if cc.device.friendly_name == 'Home TV':
-		mc = cc.media_controller
-		break
+
+mc_dict = {}
+for cc in pychromecast.get_chromecasts():
+	mc_dict[cc.device.friendly_name] = cc.media_controller
+
 
 class HelloWorld(Resource):
     def get(self):
@@ -20,9 +16,13 @@ class HelloWorld(Resource):
 
 class ToggleHomeTv(Resource):
 	def get(self):
-		# mc = self.mc
+		# Get the media controller from the dict
+		mc = mc_dict['Home TV']
+
+		# Can't call immediately for some reason, loop until we response
 		while mc.status.player_state == 'UNKNOWN':
 			continue
+
 		if mc.status.player_state == 'PAUSED':
 			mc.play()
 		elif mc.status.player_state == 'PLAYING':
@@ -33,5 +33,3 @@ api.add_resource(ToggleHomeTv, '/toggle')
 
 if __name__ == '__main__':
     app.run(debug=True)
-    # self.cc_list = pychromecast.get_chromecasts()
-    # Refresh()
